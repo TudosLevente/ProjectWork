2e1edecaba8f6e2a4b912421cef9356af3ad4943
TudosLevente <106369731+TudosLevente@users.noreply.github.com>
2024-02-01 10:36:02 +0100

    comment meg mi egyéb

---
+++ js/comment/router.js
@@ -0,0 +1,7 @@
+var comment_model = require('./comment-model');
+var router = require('express').Router();
+
+router.get('/getComment/:id',comment_model.getCommentContent);
+router.post('/uploadComment',comment_model.uploadComment);
+
+module.exports = router

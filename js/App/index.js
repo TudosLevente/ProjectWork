c56dc8860fd8bdb984ab47fb8aa6545b01b6ef82
TudosLevente <106369731+TudosLevente@users.noreply.github.com>
2024-02-06 12:13:51 +0100

    újra jó a regisztráció

--- js/App/index.js
+++ js/App/index.js
@@ -1,13 +1,12 @@
 var express = require('express');
 const cors = require('cors');
 var app = express();
-app.use(cors({origin: '*'}));
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(require('../router/router'))
-app.use('/api',require("../user/router"));
-app.use('/api',require("../login/router"));
-app.use('/api',require("../recipe/router"));
-app.use('/api',require("../comment/router"));
-
+app.use('/api', require("../user/router"));
+app.use('/api', require("../login/router"));
+app.use('/api', require("../recipe/router"));
+app.use('/api', require("../comment/router"));
 
 module.exports = app

1f968bc91ac64720a79a7d0e6650716d39e39763
TudosLevente <pop2014mmc@gmail.com>
2024-03-11 11:01:55 +0100

    recipePage comment feltöltés és megjelenítés működ ik illetve betölt mindent

--- js/comment/comment-model.js
+++ js/comment/comment-model.js
@@ -2,58 +2,51 @@ const mysql = require("mysql2");
 const config = require("../App/config");
 const Comment = require("./comment");
 
- function getCommentContent(req,res) {
+function getCommentContent(req, res) {
     var con = mysql.createConnection(config.database);
-    con.connect(function(err) {
+    con.connect(function (err) {
         if (err) throw err;
         console.log('Sikeres csatlakozás az adatbázishoz!\nJó szórakozást!');
     })
-    con.query('CALL getCommentContent(?)',[req.params['id']], (err,result) =>{
+    con.query('CALL getCommentContent(?)', [req.params['id']], (err, result) => {
         if (err) throw err;
         res.send(result);
-    })   
+    })
 }
 
-async function uploadComment (req,res)  {
+async function uploadComment(req, res) {
     const comment = new Comment(
         null,
-        null,
-        null,
+        req.body.user_id,
+        req.body.recipe_id,
         req.body.comment_text,
         null
-        );
+    );
 
     if (!(req.body.comment_text)) {
         res.status(400).send("Üresen hagytad a comment részt, tölstd ki rendesen!");
-    } 
+    }
 
     var con = mysql.createConnection(config.database);
-    con.connect(function(err) {
+    con.connect(function (err) {
         if (err) throw err;
         console.log('Sikeres csatlakozás az adatbázishoz!\nJó szórakozást!');
     })
-    
-    const sql = 'INSERT INTO Comments (Comment_Text) Values (?)';
 
-    con.query(sql,[req.body.comment_text], (err,result) =>{
-        comment.comment_id = 0;
-        comment.recipe_id = 0;
-        comment.user_id = 0;
+    const date = new Date();
 
-        const date = new Date();
+    comment.date_posted = `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}`;
 
-        comment.date_posted = `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}`;
+    const sql = 'INSERT INTO Comments (User_ID,Recipe_ID,Comment_Text,Date_Posted) Values (?,?,?,?)';
 
-        //Teszt
-        console.log(comment.date_posted);
-
-        con.connect(function(err){
+    con.query(sql, [comment.user_id, comment.recipe_id, comment.comment_text, comment.date_posted], (err, result) => {
+        con.connect(function (err) {
             if (err) throw err;
-            console.log('Sikeres feltöltés!');
+            console.log('Sikeres komment feltöltés!');
             console.log(result)
             res.send(comment);
         })
-    })   
+    })
 }
 
 exports.getCommentContent = getCommentContent
